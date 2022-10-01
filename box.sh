136df7b7a26c4e1a015e1580fe0abd984fe4d35e
Tim0n3 <>
2022-10-01 13:31:34 +0200

    fix ./box.sh: line 28: syntax error near unexpected token `)'

--- box.sh
+++ box.sh
@@ -24,22 +24,28 @@ box()
         case $option in
             h) # display switches and options
                _help
-               #exit 0;;
+               #exit 0
+               ;;
             1) # nat rules
                 # nat table function here
-               #exit 0;;
+               #exit 0
+               ;;
             2) # raw rules
             # raw table function here
-               #exit 0;;
+               #exit 0
+               ;;
             3) # mangle table
             # mangle table rules here
-               #exit 0;;
+               #exit 0
+               ;;
             4)  # filter rules
             # filter table rules here
-               #exit 0;;
+               #exit 0
+               ;;
             *) # catchall print help
                _help
-               #exit 0;;
+               #exit 0
+               ;;
         esac
     done
 }
